8d2291ed3d6c1dd22280cad8c0d9e7013c4cc637
Imane <elarkoubiimane81@gmail.com>
2025-03-19 23:03:40 +0000

    L'ajout au table produit un champ quantity_store

--- database/migrations/2022_02_11_153916_create_products_table.php
+++ database/migrations/2022_02_11_153916_create_products_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->text('description');
             $table->string('image');
             $table->integer('price');
+            $table->integer('quantity_store')
             $table->timestamps();
         });
     }
