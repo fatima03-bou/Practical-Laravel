ea4c2d2960b79a216b69ba5a3f7e286145c0ae69
zena <etajerzaina@gmail.com>
2025-04-21 16:16:03 +0100

    modifier le modele discount

--- app/Models/Discount.php
+++ app/Models/Discount.php
@@ -2,9 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Categorie;
 
 class Discount extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'type', 'rate', 'start_date', 'end_date', 'category_id', 'product_id'
+    ];
+
+    protected $casts = [
+        'start_date' => 'datetime',
+        'end_date' => 'datetime',
+        'rate' => 'float'
+    ];
+
+    public function categorie(): BelongsTo
+    {
+        return $this->belongsTo(Categorie::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function isActive(): bool
+    {
+        $now = now();
+        return $this->start_date <= $now && $this->end_date >= $now;
+    }
 }
